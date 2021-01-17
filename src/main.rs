9e242158b921ae23145fca563631e87e2b56b198
arv1ndh <arv1ndh@outlook.com>
2021-01-16 23:17:12 -0800

    Added logic to parse arguments and create word_list vector

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::{Arg, App};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
-    println!("Hello, world!");
-    let _matches = App::new("Bucket Finder tool")
+    let matches = App::new("Bucket Finder tool")
         .version("1.0")
         .author("Arvindh C. <arv1ndh@outlook.com>")
         .about("Port of bucket finder tool written by digininja")
@@ -40,4 +41,24 @@ fn main() {
              si [Singapore]
              to [Tokyo]"))
         .get_matches();
+
+    let wordlist_file = matches.value_of("wordlist").expect("Wordlist file not specified");
+    let input_file = File::open(wordlist_file).expect("File not found");
+    let file_contents = BufReader::new(input_file);
+    let mut word_list: Vec<String> = Vec::new();
+    for line in file_contents.lines() {
+        word_list.push(line.unwrap());
+    }
+    let host_prefix = "http://s3";
+    let host_suffix = "amazonaws.com";
+    let region = matches.value_of("region").unwrap_or("us");
+    let url = match region {
+        "ie" => format!("{}-eu-west-1.{}", host_prefix, host_suffix),
+        "nc" => format!("{}-us-west-1.{}", host_prefix, host_suffix),
+        "us" => format!("{}.{}", host_prefix, host_suffix),
+        "si" => format!("{}-ap-southeast-1.{}", host_prefix, host_suffix),
+        "to" => format!("{}-ap-northeast-1.{}", host_prefix, host_suffix),
+        _ => panic!("Invalid region code"),
+    };
+    println!("WordList --> {:?}", &word_list[..]);
 }
